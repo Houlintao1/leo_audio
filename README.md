931d46dae9e4accbae9d95c7839ccbe9c0c94940
houlintao <houlintao@syswin.com>
2018-07-21 13:17:32 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Leo-audio
+----------
+springboot项目
+
+email : hlt_001@126.com
--- audio/src/main/java/com/leo/audio/AudioApplication.java
+++ audio/src/main/java/com/leo/audio/AudioApplication.java
@@ -1,8 +1,13 @@
-package com.leo.audio.audio;
+package com.leo.audio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ * @author hou
+ * @date 2018年7月21日13:11:24
+ */
 @SpringBootApplication
 public class AudioApplication {
 
